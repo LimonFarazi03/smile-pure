15a7ecb2c716552b740863d95f484fa119759833
MD Limon Farazi <abidislamabir@gmail.com>
2022-04-29 12:38:17 +0600

    changes

--- src/components/Shared/Login/Login.js
+++ src/components/Shared/Login/Login.js
@@ -1,16 +1,28 @@
 import React from "react";
-import loginBanner from '../../../images/LoginRes/loginBanner.jpg';
-import './Login.css'
+import { Form } from "react-bootstrap";
+import loginBanner from "../../../images/LoginRes/loginBanner.jpg";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div className="container d-flex d-flex justify-content-around align-items-center">
-      <div className="row">
-        <div className="col-12 col-md-6 logInImg">
-          <img width={'100%'} src={loginBanner} alt="" />
+    <div className="container my-5">
+      <div className="row d-flex justify-content-between">
+        <div className="col-12 col-md-6 nonImg">
+          <img width={"105%"} src={loginBanner} alt="" />
         </div>
         <div className="col-12 col-md-6">
-          <h1>Hi my name is limon farazi</h1>
+          <div className="w-75 ms-5">
+            <form className='text-center' action="">
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label>User Name/Email</Form.Label>
+              <input className='form-control rounded-3' type="email" name="email" placeholder="email.."/>
+            </Form.Group>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <input className='form-control rounded-3' type="password" name="password" placeholder="password.."/>
+            </Form.Group>
+            </form>
+          </div>
         </div>
       </div>
     </div>
