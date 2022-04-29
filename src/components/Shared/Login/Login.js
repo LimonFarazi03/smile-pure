e1b735a99c5badf3f76d70d8432fd76b2b7f29a7
MD Limon Farazi <abidislamabir@gmail.com>
2022-04-29 20:19:19 +0600

    some essintial update done

--- src/components/Shared/Login/Login.js
+++ src/components/Shared/Login/Login.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Form } from "react-bootstrap";
 import loginBanner from "../../../images/LoginRes/loginBanner.jpg";
 import "./Login.css";
+import userIcon from "../../../images/LoginRes/user.png";
+import { Link } from "react-router-dom";
+import googleLogo from '../../../images/LoginRes/google-logo.png';
+import githubLogo from '../../../images/LoginRes/github-logo.png';
+import facebookLogo from '../../../images/LoginRes/facebook-logo.png';
 
 const Login = () => {
   return (
@@ -11,18 +15,44 @@ const Login = () => {
           <img width={"105%"} src={loginBanner} alt="" />
         </div>
         <div className="col-12 col-md-6">
-          <div className="w-75 ms-5">
-            <form className='text-center' action="">
-            <Form.Group controlId="formFile" className="mb-3">
-              <Form.Label>User Name/Email</Form.Label>
-              <input className='form-control rounded-3' type="email" name="email" placeholder="email.."/>
-            </Form.Group>
-            <Form.Group controlId="formFile" className="mb-3">
-              <Form.Label>Password</Form.Label>
-              <input className='form-control rounded-3' type="password" name="password" placeholder="password.."/>
-            </Form.Group>
-            </form>
-          </div>
+          <form>
+            <div className="p-5 loginForm">
+              <div>
+                <img src={userIcon} alt="" />
+              </div>
+              <h1 className="mt-2">Welcome back</h1>
+              <div className="mt-3">
+                <input
+                  className="mt-2 form-control"
+                  type="text"
+                  name="email"
+                  placeholder="user name/email.."
+                />
+                <input
+                  className="mt-3 form-control"
+                  type="password"
+                  name="password"
+                  placeholder="password.."
+                />
+                <div className="deco">
+                  <p className="mt-1 fw-bold">
+                    New In PureSmile?
+                    <span className="text-warning ms-3">
+                      {" "}
+                      <Link to="/signup">Create an account</Link>{" "}
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <button className="login_btn btn btn-primary">Login</button>
+              <hr/>
+              <div className="d-flex logo-Img">
+                <img src={googleLogo} alt="" />
+                <img src={githubLogo} alt="" />
+                <img src={facebookLogo} alt="" />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/Shared/Signup/Signup.js
+++ src/components/Shared/Signup/Signup.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import userIcon from "../../../images/LoginRes/user.png";
+import { Link } from "react-router-dom";
+import loginBanner from "../../../images/LoginRes/loginBanner.jpg";
+import googleLogo from '../../../images/LoginRes/google-logo.png';
+import githubLogo from '../../../images/LoginRes/github-logo.png';
+import facebookLogo from '../../../images/LoginRes/facebook-logo.png';
+import './Signup.css';
 
 const Signup = () => {
   return (
-    <div>
-      <h1>This is signup</h1>
+    <div className="container my-5">
+      <div className="row d-flex justify-content-between">
+        <div className="col-12 col-md-6 nonImg">
+          <img width={"105%"} src={loginBanner} alt="" />
+        </div>
+        <div className="col-12 col-md-6">
+          <form>
+            <div className="p-5 loginForm">
+              <div>
+                <img src={userIcon} alt="" />
+              </div>
+              <h1 className="mt-2">Register Now</h1>
+              <div className="mt-3">
+                <input
+                  className="mt-2 form-control"
+                  type="text"
+                  name="email"
+                  placeholder="user name/email.."
+                />
+                <input
+                  className="mt-3 form-control"
+                  type="password"
+                  name="password"
+                  placeholder="password.."
+                />
+                <input
+                  className="mt-3 form-control"
+                  type="password"
+                  name="password"
+                  placeholder="Confirm password.."
+                />
+                <div className="deco">
+                  <p className="mt-1 fw-bold">
+                    Already have and account?
+                    <span className="text-warning ms-3">
+                      {" "}
+                      <Link to="/login">Login</Link>{" "}
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <button className="login_btn btn btn-primary">Login</button>
+              <hr/>
+              <div className="d-flex logo-Img">
+                <img src={googleLogo} alt="" />
+                <img src={githubLogo} alt="" />
+                <img src={facebookLogo} alt="" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
