8f78e5b98db41f756d5bd50c1a10283aa641c41e
MD Limon Farazi <abidislamabir@gmail.com>
2022-04-30 00:00:13 +0600

    signout btn added

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../../../images/small-picture/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CustomLink from "../CustomLink/CustomLink";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
@@ -10,11 +10,10 @@ import { signOut } from "firebase/auth";
 
 const Header = () => {
   const [user, loading, error] = useAuthState(auth);
-  
   const logout = () => {
     signOut(auth);
   };
-  console.log(user)
+
   return (
     <div className="bg-white bg-shadow sticky-top">
       <Navbar expand="lg">
@@ -31,7 +30,7 @@ const Header = () => {
               <CustomLink to="/about">About</CustomLink>
               <CustomLink to="/login">Login</CustomLink>
               <div className="signup-btn">
-              {user? <CustomLink to="/signup">Logout</CustomLink>:<CustomLink to="/signup">Signup</CustomLink>}
+              {user? <p className="logout-btn" onClick={logout}>Logout</p>:<CustomLink to="/signup">Signup</CustomLink>}
               </div>
             </Nav>
           </Navbar.Collapse>
